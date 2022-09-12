86fd7c9dd99537a58311f1b99a95f3b282f20210
jkelly3rd <jkelly3rd@gmail.com>
2022-09-12 11:13:31 -0400

    Update homicide page/code

--- scripts/process_houston_crime_data.R
+++ scripts/process_houston_crime_data.R
@@ -518,28 +518,28 @@ when_murders_happen %>% write_csv("data/output/when_murders_happen.csv")
 # Calculating some murder frequency figures for the homicides page story element
 beatpops_down <- beats %>% st_drop_geometry() %>% select(1,5) %>% 
   left_join(murders_beat) %>% 
-  filter(rate_last12<=rate_prior3years) %>%
+  filter(rate_last12<=14.4) %>%
   select(1,2,23,24)
 beatpops_down$diff <- beatpops_down$rate_last12-beatpops_down$rate_prior3years
 pop_murdersdown <- sum(beatpops_down$population)
 # population where murders are up 20%
 beatpops_up <- beats %>% st_drop_geometry() %>% select(1,5) %>% 
   left_join(murders_beat) %>% 
-  filter(rate_last12>=(1.2*rate_prior3years)) %>%
+  filter(rate_last12>=14.4) %>%
   select(1,2,23,24)
 pop_murdersup <- sum(beatpops_up$population)
 # population where there were zero murders
-beatpops_zero <- beats %>% st_drop_geometry() %>% select(1,5) %>% 
-  left_join(murders_beat) %>% 
-  filter(last12mos<=0) %>%
-  select(1,2)
-pop_murderszero <- sum(beatpops_zero$population)
+#beatpops_zero <- beats %>% st_drop_geometry() %>% select(1,5) %>% 
+#  left_join(murders_beat) %>% 
+#  filter(last12mos<=0) %>%
+#  select(1,2)
+#pop_murderszero <- sum(beatpops_zero$population)
 # population where there was at least one murder every month
-beatpops_monthly <- beats %>% st_drop_geometry() %>% select(1,5) %>% 
-  left_join(murders_beat) %>% 
-  filter(last12mos>=12) %>%
-  select(1,2)
-pop_murdersmonthly <- sum(beatpops_monthly$population)
+# beatpops_monthly <- beats %>% st_drop_geometry() %>% select(1,5) %>% 
+#  left_join(murders_beat) %>% 
+#  filter(last12mos>=12) %>%
+#  select(1,2)
+# pop_murdersmonthly <- sum(beatpops_monthly$population)
 # cleanup
 rm(beatpops_down,beatpops_up,beatpops_monthly,beatpops_zero)
 
