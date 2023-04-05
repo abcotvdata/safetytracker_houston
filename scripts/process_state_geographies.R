bd1f94c75e232cca970717f00fbcbf3b9eb8f064
jkelly3rd <jkelly3rd@gmail.com>
2023-04-05 13:04:23 -0400

    a

--- scripts/process_state_geographies.R
+++ scripts/process_state_geographies.R
@@ -49,7 +49,9 @@ tx_counties$county <- str_replace(tx_counties$county," County, Texas","")
 # Assign county names for filter and temporarily import Cal DOJ list for filter
 counties <- c("Harris", "Montgomery", "Liberty", "Waller", "Fort Bend", "Brazoria", "Galveston", "Chambers")
 
-socal_annual <- readRDS("scripts/rds/socal_annual.rds")
+#### OPEN WORK TO GET A CRIME FILE HERE FOR TEXAS
+#ourtx_annual <- readRDS("scripts/rds/ourtx_annual.rds")
+
 # Apply filters to assign places to counties, filter for counties and state data agencies
 ourtx_places <- st_join(tx_places, tx_counties %>% select(5), left = FALSE, largest = TRUE) %>%
   filter(county %in% counties)
@@ -64,69 +66,85 @@ all_ourtx_places <- ourtx_places %>%
   ungroup() %>% st_make_valid()
 
 # Create five single polygon county files
-riverside_county <- tx_counties %>% filter(county=="Riverside County") %>% st_make_valid()
-orange_county <- tx_counties %>% filter(county=="Orange County") %>% st_make_valid()
-sanbern_county <- tx_counties %>% filter(county=="San Bernardino County") %>% st_make_valid()
-ventura_county <- tx_counties %>% filter(county=="Ventura County") %>% st_make_valid()
+harris_county <- tx_counties %>% filter(county=="Harris") %>% st_make_valid()
+montgomery_county <- tx_counties %>% filter(county=="Montgomery") %>% st_make_valid()
+liberty_county <- tx_counties %>% filter(county=="Liberty") %>% st_make_valid()
+waller_county <- tx_counties %>% filter(county=="Waller") %>% st_make_valid()
+fortbend_county <- tx_counties %>% filter(county=="Fort Bend") %>% st_make_valid()
+brazoria_county <- tx_counties %>% filter(county=="Brazoria") %>% st_make_valid()
+galveston_county <- tx_counties %>% filter(county=="Galveston") %>% st_make_valid()
+chambers_county <- tx_counties %>% filter(county=="Chambers") %>% st_make_valid()
+
 
 # Make the rural "remnant" area polygons for each county
-rural_riverside <- st_difference(riverside_county,all_sotx_places)
-rural_orange <- st_difference(orange_county,all_sotx_places)
-rural_sanbern <- st_difference(sanbern_county,all_sotx_places)
-rural_ventura <- st_difference(ventura_county,all_sotx_places)
+rural_harris <- st_difference(harris_county,all_ourtx_places)
+rural_montgomery <- st_difference(montgomery_county,all_ourtx_places)
+rural_liberty <- st_difference(liberty_county,all_ourtx_places)
+rural_waller <- st_difference(waller_county,all_ourtx_places)
+rural_fortbend <- st_difference(fortbend_county,all_ourtx_places)
+rural_brazoria <- st_difference(brazoria_county,all_ourtx_places)
+rural_galveston <- st_difference(galveston_county,all_ourtx_places)
+rural_chambers <- st_difference(chambers_county,all_ourtx_places)
 
 # Make the rural "remnant" area polygons for each county
-rural_riverside$place <- "Riverside Co. Sheriff's Department"
-rural_orange$place <- "Orange Co. Sheriff's Department"
-rural_sanbern$place <- "San Bernardino Co. Sheriff's Department"
-rural_ventura$place <- "Ventura Co. Sheriff's Department"
+rural_harris$place <- "Harris Co. Sheriff's Department"
+rural_montgomery$place <- "Montgomery Co. Sheriff's Department"
+rural_liberty$place <- "Liberty Co. Sheriff's Department"
+rural_waller$place <- "Waller Co. Sheriff's Department"
+rural_fortbend$place <- "Fort Bend Co. Sheriff's Department"
+rural_brazoria$place <- "Brazoria Co. Sheriff's Department"
+rural_galveston$place <- "Galveston Co. Sheriff's Department"
+rural_chambers$place <- "Chambers Co. Sheriff's Department"
 
 # Make the rural "remnant" area polygons for each county
-socal_county_pops <- sotx_places %>% group_by(county) %>% summarise(pop=sum(population))
-rural_riverside$population <- riverside_county$population - 2027000
-rural_orange$population <- orange_county$population - 3053900
-rural_sanbern$population <- sanbern_county$population - 1888800
-rural_ventura$population <- ventura_county$population - 750000
+ourtx_county_pops <- ourtx_places %>% group_by(county) %>% summarise(pop=sum(population))
+rural_harris$population <- harris_county$population - 2027000
+rural_montgomery$population <- montgomery_county$population - 3053900
+rural_liberty$population <- liberty_county$population - 1888800
+rural_waller$population <- waller_county$population - 750000
+rural_fortbend$population <- fortbend_county$population - 2027000
+rural_brazoria$population <- brazoria_county$population - 3053900
+rural_galveston$population <- galveston_county$population - 1888800
+rural_chambers$population <- chambers_county$population - 750000
 
-# Add these rural sheriff's coverage areas back into sotx_places
-sotx_places <- rbind(sotx_places,rural_riverside,rural_orange,rural_sanbern,rural_ventura)
+# Add these rural sheriff's coverage areas back into ourtx_places
+ourtx_places <- rbind(ourtx_places,rural_harris,rural_montgomery,rural_liberty,rural_waller,rural_fortbend,
+                      rural_brazoria,rural_galveston,rural_chambers) %>%
+  st_transform(4326)
 
 # Do some cleanup 
-rm(rural_riverside,rural_orange,rural_sanbern,rural_ventura)
-rm(riverside_county,orange_county,sanbern_county,ventura_county)
-rm(tx_counties, tx_places, all_sotx_places,socal_county_pops)
+rm(rural_harris,rural_montgomery,rural_liberty,rural_waller,rural_fortbend,rural_brazoria,rural_galveston,rural_chambers)
+rm(harris_county,montgomery_county,liberty_county,waller_county,fortbend_county,brazoria_county,galveston_county,chambers_county)
+rm(tx_counties, tx_places, all_ourtx_places, ourtx_county_pops)
 
 # Create new police_map
 # la_districts <- readRDS(data/rds/la_county_police_districts.rds)
 # Remove all LA County places from the so_cal places now
-police_map <- bind_rows(sotx_places %>% filter(county!="Los Angeles County"),la_districts)
+police_map <- bind_rows(ourtx_places %>% filter(place!="Houston"),beats)
 # Add upgraded LA County districts to the new region wide police districts map
-police_map$county <- ifelse(is.na(police_map$county),"Los Angeles County",police_map$county)
+police_map$county <- ifelse(is.na(police_map$county),"Harris",police_map$county)
 # Recode place field for all the LA county policing districts
-police_map$place <- case_when(police_map$agency=="OTHER" & police_map$commtype=="City" ~ police_map$place_name,
-                              police_map$agency=="LASD" & police_map$commtype=="City" ~ police_map$place_name,
-                              police_map$agency=="LASD" & police_map$commtype=="Unincorporated" ~ paste(police_map$place_name, police_map$district),
-                              police_map$agency=="LAPD" ~ paste(police_map$agency, police_map$district),
-                              TRUE ~ police_map$place)
-
+#police_map$place <- case_when(police_map$agency=="OTHER" & police_map$commtype=="City" ~ police_map$place_name,
+#                              police_map$agency=="LASD" & police_map$commtype=="City" ~ police_map$place_name,
+#                              police_map$agency=="LASD" & police_map$commtype=="Unincorporated" ~ paste(police_map$place_name, police_map$district),
+#                              police_map$agency=="LAPD" ~ paste(police_map$agency, police_map$district),
+#                              TRUE ~ police_map$place)
 
 # Set bins for beats pop map
 popbins <- c(0,1000,5000,25000,50000,75000,100000,125000,150000,175000,200000,Inf)
 poppal <- colorBin("viridis", police_map$population, bins = popbins)
-poplabel <- paste(sep = "<br>", police_map$place,police_map$county,police_map$agency,police_map$commtype,prettyNum(police_map$population, big.mark = ","))
+poplabel <- paste(sep = "<br>", police_map$place,police_map$county,prettyNum(police_map$population, big.mark = ","))
 # Create map
-socal_police_map <- leaflet(police_map) %>%
-  setView(-117.243, 33.70, zoom = 10) %>% 
+ourtx_police_map <- leaflet(police_map) %>%
+  setView(-95.45, 29.75, zoom = 9) %>% 
   addProviderTiles(provider = "Esri.WorldImagery") %>%
   addProviderTiles(provider = "CartoDB.PositronOnlyLabels") %>%
   addPolygons(color = "white", popup = poplabel, weight = 2, smoothFactor = 0.5,
               opacity = 0.5, fillOpacity = 0.5,
               fillColor = ~poppal(`population`)) 
-socal_police_map 
+ourtx_police_map 
 
 # Saving final product for reuse; police map includes 4 counties + LA Co
 saveRDS(police_map,"scripts/rds/police_map.rds")
-# Saving the file with only the police districts in the 4 counties besides LA too
-saveRDS(sotx_places,"scripts/rds/police_map.rds")
 
 rm(socal_police_map)
