091bd294a621b17a1b547ee49c56b9ad3978f5f2
lindseyf96 <39309451+lindseyf96@users.noreply.github.com>
2024-03-29 18:17:53 -0700

    Update process_monthly.R

--- scripts/process_monthly.R
+++ scripts/process_monthly.R
@@ -12,21 +12,41 @@ links <- html_attr(a_tags, "href")
 links <- grep("xls", links, value = TRUE)
 # the first xls file will always be the file we want
 # download the file to the source data directory; building the url in process
-try(download.file(paste0("https://www.houstontx.gov/police/cs/",links[[1]]),
-              "data/source/annual/houston_NIBRS2023.xlsx"))
+# try(download.file(paste0("https://www.houstontx.gov/police/cs/",links[[1]]),
+#              "data/source/annual/houston_NIBRS2023.xlsx"))
+#
+# houston23 <- read_excel("data/source/annual/houston_NIBRS2023.xlsx", 
+#                        col_types = c("text", "date", "numeric", 
+#                                      "text" , "text", "numeric", "text", 
+#                                      "text", "text", "text", "text", "text", 
+#                                      "text", "text","numeric","numeric"))
+# names(houston23) <- c("incident", "date", "hour", 
+#                      "nibrs_class", "offense_type", "offense_count", "beat", 
+#                      "premise", "street_no", "street_name", "street_type", "street_suffix", 
+#                      "city", "zip","longitude","latitude")
 
-houston23 <- read_excel("data/source/annual/houston_NIBRS2023.xlsx", 
+# Save as RDS for use in other tracker scripts; results in 4.2MB RDS
+# saveRDS(houston23,"scripts/rds/houston_monthly.rds")
+
+# Clean up
+# rm(houston23,webpage,a_tags,links)
+
+
+try(download.file(paste0("https://www.houstontx.gov/police/cs/",links[[6]]),
+              "data/source/annual/houston_NIBRS2024.xlsx"))
+
+houston24 <- read_excel("data/source/annual/houston_NIBRS2024.xlsx", 
                         col_types = c("text", "date", "numeric", 
                                       "text" , "text", "numeric", "text", 
                                       "text", "text", "text", "text", "text", 
                                       "text", "text","numeric","numeric"))
-names(houston23) <- c("incident", "date", "hour", 
+names(houston24) <- c("incident", "date", "hour", 
                       "nibrs_class", "offense_type", "offense_count", "beat", 
                       "premise", "street_no", "street_name", "street_type", "street_suffix", 
                       "city", "zip","longitude","latitude")
 
 # Save as RDS for use in other tracker scripts; results in 4.2MB RDS
-saveRDS(houston23,"scripts/rds/houston_monthly.rds")
+saveRDS(houston24,"scripts/rds/houston_monthly.rds")
 
 # Clean up
-rm(houston23,webpage,a_tags,links)
+rm(houston24,webpage,a_tags,links)
